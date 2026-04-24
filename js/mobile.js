27532a358f81d6476dc40d8d3850566324b7571e
Nilsonmarcel <nilsonmarcel@gmail.com>
2026-04-24 16:52:28 -0300

    feat: ajustes e maps

--- js/mobile.js
+++ js/mobile.js
@@ -1,5 +1,7 @@
 const btnOpenMenuMobile = document.querySelector('#btn-menu-mobile');
 const menuMobile = document.querySelector('#menu-mobile');
+const btnOpenBoxMaps = document.querySelector('#btn-box-maps');
+const boxMaps = document.querySelector('#box-maps');
 
 btnOpenMenuMobile.addEventListener('click', () => {
     menuMobile.classList.add('open-menu');
@@ -7,4 +9,12 @@ btnOpenMenuMobile.addEventListener('click', () => {
 
 menuMobile.addEventListener('click', () => {
         menuMobile.classList.remove('open-menu');    
+});
+
+btnOpenBoxMaps.addEventListener('click', () => {
+    boxMaps.classList.add('open-maps');
+});
+
+boxMaps.addEventListener('click', () => {
+        boxMaps.classList.remove('open-maps');    
 })
